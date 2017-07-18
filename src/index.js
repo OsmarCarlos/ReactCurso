2fe57b251f34c5dd12f033b408a32ef9beb311ad
Carlos <orodriguez@mimoni.com>
2017-07-18 11:26:49 -0500

    import same components to index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Name from './Components/name';
+import Hora from './Components/hora';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+
+ReactDOM.render(<Name />, document.getElementById('name'));
+
+ReactDOM.render(<Hora />, document.getElementById('hora'));
+
 registerServiceWorker();
