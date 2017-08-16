f9e1e2baebb3a011be519806baa02cac24cf4ba0
Osmar Carlos <stoke07@live.com>
2017-08-15 23:21:56 -0500

    Fragment my component

--- src/Components/form.js
+++ src/Components/form.js
@@ -6,15 +6,12 @@ class Form extends Component {
         super(props);
         this.state = {
             firsName: '',
-            secondName: ''
+            secondName: '',
+            data: []
         };
 
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
-    }
-
-    concat(user) {
-        return user.firsName + ' ' + user.secondName
     }
 
     handleChange (event) {
@@ -29,6 +26,14 @@ class Form extends Component {
         alert('Tu nombre fue subido '+ this.state.firsName + ' ' + this.state.secondName)
         event.preventDefault();
         console.log(this.state)
+        var object = {
+            firsName: this.state.firsName,
+            secondName: this.state.secondName
+        }
+        this.setState({
+            data: this.state.data.concat([object])
+        })
+        console.log(this.data)
     }
 
 
@@ -45,10 +50,29 @@ class Form extends Component {
                         <input type= "text" name="secondName" value={this.state.secondName} onChange= {this.handleChange} />
                     </label>
                     <input type="submit" value="Subir"/>
-                    <p>Hola {this.concat(this.state)}</p>
                 </form> 
+                {this.state.data.map((person, i) => <Print key={i} personData = {person}/>)}
             </div>
         );
+    }
+}
+
+class Print extends Component {
+
+    concat(user) {
+        return user.firsName + ' ' + user.secondName
+    }
+
+    render() { 
+        var style = {
+            fontSize: 20,
+            color: 'red'
+        };
+        return(
+            <div>
+                <p style={style}>Hola {this.concat(this.props.personData)}</p>
+            </div>
+        )
     }
 }
 
